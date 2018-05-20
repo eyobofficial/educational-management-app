d40727600d4020dc5980c1fbe3570cf9a142c806
eyob tariku <eyobtariku@gmail.com>
2018-05-21 00:16:27 +0300

    Complete index.html dashboard template prototype

--- static/app-assets/js/scripts/charts/chartjs/bar/bar.js
+++ static/app-assets/js/scripts/charts/chartjs/bar/bar.js
@@ -21,57 +21,70 @@ $(window).on("load", function(){
         // In this case, we are setting the border of each horizontal bar to be 2px wide and green
         elements: {
             rectangle: {
-                borderWidth: 2,
+                borderWidth: 1,
                 borderColor: 'rgb(0, 255, 0)',
                 borderSkipped: 'left'
             }
         },
+        barThickness: 1,
         responsive: true,
         maintainAspectRatio: false,
         responsiveAnimationDuration:500,
         legend: {
-            position: 'top',
+            position: 'bottom',
         },
         scales: {
             xAxes: [{
                 display: true,
+                fontWeight: 600,
                 gridLines: {
                     color: "#f3f3f3",
-                    drawTicks: false,
+                    drawTicks: false
                 },
                 scaleLabel: {
-                    display: true,
+                    display: false,
+                },
+                ticks: {
+                    beginAtZero:true,
+                    max: 100,
                 }
             }],
             yAxes: [{
                 display: true,
+                fontWeight: 600,
+                barThickness: 40,
+                categoryPercentage: 0.4,
                 gridLines: {
                     color: "#f3f3f3",
-                    drawTicks: false,
+                    drawTicks: false
                 },
                 scaleLabel: {
-                    display: true,
+                    display: false,
+                },
+                ticks: {
+                    beginAtZero:true,
+                    max: 100
                 }
             }]
         },
         title: {
             display: false,
-            text: 'Chart.js Horizontal Bar Chart'
+            text: 'Current Subject Mark'
         }
     };
 
     // Chart Data
     var chartData = {
-        labels: ["January", "February", "March", "April"],
+        labels: [""],
         datasets: [{
-            label: "My First dataset",
-            data: [65, 59, 80, 81],
+            label: "Student Mark",
+            data: [65],
             backgroundColor: "#28D094",
             hoverBackgroundColor: "rgba(22,211,154,.9)",
             borderColor: "transparent"
         }, {
-            label: "My Second dataset",
-            data: [28, 48, 40, 19],
+            label: "Subject Average Mark",
+            data: [30],
             backgroundColor: "#F98E76",
             hoverBackgroundColor: "rgba(249,142,118,.9)",
             borderColor: "transparent"
