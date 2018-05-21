3d307d72cef2baac8d20e9d718d444fb9eb6232a
eyob tariku <eyobtariku@gmail.com>
2018-05-22 00:09:32 +0300

    Do fixed sidebar and chat content template in teams tab

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,3 +8,21 @@ class IndexView(TemplateView):
 
 class TeamView(TemplateView):
     template_name = 'dashboard/teams.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['channel_list'] = [
+            'Subject 1',
+            'Mid Exam',
+            'Assignment Group',
+        ]
+        context['student_list'] = [
+            'William',
+            'Kavin Hart',
+            'Instructor',
+            'Jay Z',
+            'Eminem',
+            'Will Smith',
+        ]
+        context['msg_counts'] = [3, 5, 10, 1, 7, ]
+        return context
