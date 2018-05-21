0c79a8b7a4bef528d40beafd2cb29e9667b78cf8
Eyob Tariku <eyobtariku@gmail.com>
2018-05-21 14:50:17 +0300

    Add url links to navigation anchor tags

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,5 +6,5 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('teams/', views.TeamView.as_view(), name='team'),
+    path('teams/', views.TeamView.as_view(), name='teams'),
 ]
